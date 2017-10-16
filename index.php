7c99f4d8113e606e5acc821078adbd75ffb855ff
kaleniuk2 <guilhermekaleniuk@hotmail.com>
2017-10-16 00:42:57 -0200

    fizemos o admin

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use sampacapoeira\Page;
-
+use sampacapoeira\PageAdmin;
 
 
 $app = new Slim();
@@ -14,6 +14,13 @@ $app->config('debug', true);
 $app->get('/', function() {
     
 	$page = new Page();
+	$page->setTpl("index");
+
+});
+
+$app->get('/admin', function() {
+    
+	$page = new PageAdmin();
 	$page->setTpl("index");
 
 });
